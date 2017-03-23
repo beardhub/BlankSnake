40719286dacccd6716a30c8a9f536aada4de341a
Zack Ervin <zjervin@gmail.com>
2017-03-23 17:22:52 -0400

    Added comments

--- script.js
+++ script.js
@@ -1,117 +1,120 @@
-window.onload = init;
-var c;
-var snake;
-var dt, db, t;
+window.onload = init;													//Execute 'init' once everything is loaded
+var canvas;
+var snake;																//Global variables for the game
+var tileSize, boardDim;
 function init(){
-	c = document.getElementById("canvas");
-	t = 20;
-	db = c.width;
-	dt = db/t;
-	snake = new Snake();
-	snake.init();
-	document.addEventListener("keydown",snake.keydown.bind(snake));
-	gameloop();
+	canvas = document.getElementById("canvas");							//Get canvas from HTML and store it in canvas var
+	boardDim = 20;														//20 tile x 20 tile board
+	tileSize = canvas.width/boardDim;									//Size in pixels of each tile
+	snake = new Snake();												//Initialize game
+	snake.init();														//Start game
+	document.addEventListener("keydown",snake.keydown.bind(snake));		//Send keyboard events from the document to the game
+	gameloop();															//Start the game loop
 }
 function gameloop(){
-	var g = c.getContext("2d");
-	prepScreen(g);
-	snake.update();
-	snake.render(g);
-	requestAnimationFrame(gameloop);
+	var g = canvas.getContext("2d");									//Get the graphics object of the canvas for drawing
+	prepScreen(g);														//Preps the graphics object for the game to render
+	snake.update();														//Update the game
+	snake.render(g);													//Passes the graphics object to the game for rendering
+	requestAnimationFrame(gameloop);									//Call gameloop again once the webpage is ready to render again
 }
 function prepScreen(g){
-	g.clearRect(0,0,c.width,c.height);
-	g.strokeStyle = "black";
-	g.lineWidth = 8;
-	g.strokeRect(0,0,c.width,c.height);
-	g.lineWidth = 1;
-	for (var i = 0; i < t; i++)
-		for (var j = 0; j < t; j++)
-			g.strokeRect(dt*i,dt*j,dt,dt);
+	g.clearRect(0,0,canvas.width,canvas.height);						//Clear all graphics, without this the old frames would still be rendered
+	g.strokeStyle = "black";											//Change the drawing color to black
+	g.lineWidth = 8;													//You can guess
+	g.strokeRect(0,0,canvas.width,canvas.height);						//Draw the outline of the board
+	g.lineWidth = 1;													
+	for (var i = 0; i < boardDim; i++)									//Iterate across every 'tile' on the board
+		for (var j = 0; j < boardDim; j++)
+			g.strokeRect(tileSize*i,tileSize*j,tileSize,tileSize);		//and draw the outline of a square around it to form the grid
 }
 function Snake(){
-	function Seg(x,y){
-		this.x = x;
+	function Tile(x,y){													//Object representing a tile on the board
+		this.x = x;														//Set x and y coordinates
 		this.y = y;
-		this.eq = function(o){
-			return o.x==this.x&&o.y==this.y;
+		this.equals = function(other){									//Function to check if this tile is equal to another tile
+			return other.x == this.x &&
+				   other.y == this.y;
 		}
 	}
-	this.init = function(){
-		this.wait = 0;
-		this.segs = [new Seg(Math.round(t/2),Math.round(t/2))];
-		this.dir = -1;
-		this.spawnfood();
-		this.p = "";
+	this.init = function(){												//Start game
+		this.cooldown = 0;												//Delay in frames before moving to next tile
+		this.segments = [new Tile(Math.round(boardDim/2),				//List of all the tiles in the snake
+								  Math.round(boardDim/2))];
+		this.direction = -1;											//Direction travelling. 0 = up, 1 = left, 2 = down, 3 = right, -1 = not moving
+		this.spawnfood();												//Spawn the first food thing
+		this.pressed = "";												//The most recent key pressed, only changes for 'w' 'a' 's' and 'd'
 	}
 	this.update = function(){
-		if (this.p == "")
+		if (this.pressed == "")											//If no direction has been pressed down update
 			return;
-		this.wait--;
-		if (this.wait <= 0){
-			this.wait = 10;
+		this.cooldown--;												//Decrement the delay
+		if (this.cooldown <= 0){										//When the delay reaches 0, reset the delay and move one tile
+			this.cooldown = 10;
 			this.move();
 		}
 	}
 	this.render = function(g){
 		g.fillStyle = "black";
-		for (var i = 0; i < this.segs.length; i++)
-			g.fillRect(this.segs[i].x*dt,this.segs[i].y*dt,dt,dt);
-		g.fillStyle = "red";
-		g.fillRect(this.food.x*dt,this.food.y*dt,dt,dt);
+		for (var i = 0; i < this.segments.length; i++)					//Draw all the tiles in the snake based on the coordinates and the size of each tile
+			g.fillRect(this.segments[i].x*tileSize,this.segments[i].y*tileSize,tileSize,tileSize);
+		g.fillStyle = "red";											//Change the color to red and draw the food
+		g.fillRect(this.food.x*tileSize,this.food.y*tileSize,tileSize,tileSize);
 	}
 	this.keydown = function(k){
-		switch(k.code){
-			case "KeyW":case "KeyA":case "KeyS":case "KeyD":break;
-			default:return;
+		switch(k.code){													//Get the code from the key event passed from the document
+			case "KeyW":case "KeyA":case "KeyS":case "KeyD":			//If it is a valid key change this.pressed, otherwise do nothing
+				this.pressed = k.code;
+			break;
 		}
-		this.p = k.code;
 	}
 	this.spawnfood = function(){
 		var overlap = false;
 		do{
-			overlap = false;
-			this.food = new Seg(Math.round(Math.random()*(t-1)),Math.round(Math.random()*(t-1)));
-			for (var i = 0; i < this.segs.length; i++)
-				if (this.segs[i].eq(this.food))
+			overlap = false;											//Move the food to a random location on the board
+			this.food = new Tile(Math.round(Math.random()*(boardDim-1)),Math.round(Math.random()*(boardDim-1)));
+			for (var i = 0; i < this.segments.length; i++)				//Check if it overlaps the snake at any point and
+				if (this.segments[i].equals(this.food))					//If so find a new location, repeating until it doesnt overlap
 					overlap = true;
 		}while(overlap);
 	}
-	this.collide = function(h){
-		if (h.x<0||h.x>=t||h.y<0||h.y>=t)
+	this.collide = function(){											//Checks if a collision has been triggered
+		var h = this.getHead();											//Get the head of the snake
+		if (h.x<0||h.x>=boardDim||h.y<0||h.y>=boardDim)					//Return true if the head of the snake is off the board
 			return true;
-		for (var i = 0; i < this.segs.length-1; i++)
-			if (h.eq(this.segs[i]))
+		for (var i = 0; i < this.segments.length-1; i++)				//Iterate through all of the tiles in the snake except the head
+			if (h.equals(this.segments[i]))								//And check if the head is in the same position, meaning it hit itself
 				return true;
-		return false;
+		return false;													//Return no collision if the above didnt return true
 	}
-	var head = function(){
-		return this.segs[this.segs.length-1];
-	}.bind(this);
+	this.getHead = function(){											//Function to get the head of the snake
+		return this.segments[this.segments.length-1];
+	}
 	this.move = function(){
 		var d = 0;
-		switch (this.p){
+		switch (this.pressed){											//Decide a direction based on the last key pressed
 			case "KeyW":	d = 0;	break;
 			case "KeyA":	d = 1;	break;
 			case "KeyS":	d = 2;	break;
 			case "KeyD":	d = 3;	break;
 			default:	return;
 		}
-		if (Math.abs(this.dir-d)%2!=0 || this.segs.length == 1)
-			this.dir = d;
-		var next;
-		var h = head();
-		switch(this.dir){
-			case 0:		next = new Seg(h.x,		h.y-1);	break;
-			case 1:		next = new Seg(h.x-1,	h.y);	break;
-			case 2:		next = new Seg(h.x,		h.y+1);	break;
-			case 3:		next = new Seg(h.x+1,	h.y);	break;
+		if (Math.abs(this.direction-d)%2!=0 || this.segments.length==1)	//If the player isnt trying to go straight forward or straight backward,
+			this.direction = d;											//OR the snake is length 1, set the travel direction to that direction
+			
+		var h = this.getHead();
+		var nextHead;													//Tile representing the next place the head is going to be
+		switch(this.direction){
+			case 0:		nextHead = new Tile(h.x,	h.y-1);	break;		//Make a new tile based on the direction the snake is headed
+			case 1:		nextHead = new Tile(h.x-1,	h.y);	break;
+			case 2:		nextHead = new Tile(h.x,	h.y+1);	break;
+			case 3:		nextHead = new Tile(h.x+1,	h.y);	break;
 		}
-		this.segs.push(next);
-		if (!next.eq(this.food))
-			this.segs.splice(0,1);
-		else this.spawnfood();
-		if (this.collide(head()))
+		this.segments.push(nextHead);									//Add the new head onto the snake
+		if (!nextHead.equals(this.food))								//If the snake didnt just eat food remove the end of its tail to move forward
+			this.segments.splice(0,1);									//Otherwise leave the tail alone and allow the snake to grow by one
+		else this.spawnfood();											//If it did eat the food move the food to a new location
+		if (this.collide())												//If there is a collision start the game over
 			this.init();
 	}
 }
